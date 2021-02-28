2b05dec47a377a480e54cf14e60b07418a013700
Azim-Ahmed <azimuahmed13@gmail.com>
2021-02-28 21:34:15 +0600

    added syntex to the repo / learn syntax of python

---
+++ syntex/variables.py
@@ -0,0 +1,9 @@
+# In Python, variables are created when you assign a value to it:
+x = 5
+y = "Hello, World!"
+
+# Python has commenting capability for the purpose of in-code documentation.
+
+# Comments start with a #, and Python will render the rest of the line as a comment:
+#This is a comment.
+print("Hello, World!")
