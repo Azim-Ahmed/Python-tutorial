5a0996cba5f870f48e3aa9deec2e7b92d9fb3cd3
Azim-Ahmed <azimuahmed13@gmail.com>
2021-03-03 18:34:51 +0600

    Arithmatic operator added to this tutorial

---
+++ operator/arithmatic.py
@@ -0,0 +1,8 @@
+a , b = 35, 10
+
+print(a+b)    #45
+print(a-b)    #25
+print(a*b)    #350
+print(a/b)    #3.5
+print(a//b)    #3 (this will print only floor rest of number)
+print(a%b)    #0 ( modulus or reminder operator)
---
+++ operator/index.py
@@ -0,0 +1,11 @@
+# What is Operator?
+# Opeators are special symbols which perform 
+#different computation in python
+
+#Arithmatic (patigonit)
+#Comparisonal or relational operator
+# Logical
+#Bitwise
+#Assignment
+#Identity
+#Membership 
