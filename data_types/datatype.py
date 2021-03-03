78a9c522e983d21788a3d22a596b52d48abe03ca
Azim-Ahmed <azimuahmed13@gmail.com>
2021-03-03 17:55:46 +0600

    python data type added to this tutorial

---
+++ data_types/datatype.py
@@ -0,0 +1,11 @@
+# In python, everything is an object
+name = "Azim"
+a = 0 
+m = 1.0
+c = 1+5j # in python we use complex number in j
+b  = True
+print(type(name))
+print(type(a))
+print(type(m))
+print(type(c))
+print(type(b))
