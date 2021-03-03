4cd625e8dce5a8cd3e6a4d2be17a6000b830247f
Azim-Ahmed <azimuahmed13@gmail.com>
2021-03-03 17:45:18 +0600

    style guide added to this tutorial

--- variables/variableNames.py
+++ variables/variableNames.py
@@ -4,3 +4,8 @@
 # A variable name cannot start with a number
 # A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 # Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+# constant variable convention is writing code to in the upper case
+
+PI = 4.1416
+print(PI)
