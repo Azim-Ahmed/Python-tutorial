4a38112effbf68747606bbe0d7f30ce5ff297f17
Azim-Ahmed <azimuahmed13@gmail.com>
2021-03-03 18:13:10 +0600

    python all kind of data type added to this tutorial

--- data_types/datatype.py
+++ data_types/datatype.py
@@ -4,8 +4,39 @@ a = 0
 m = 1.0
 c = 1+5j # in python we use complex number in j
 b  = True
+
+#these all are base data type
 print(type(name))
 print(type(a))
 print(type(m))
 print(type(c))
 print(type(b))
+
+#output
+# <class 'str'>
+# <class 'int'>    
+# <class 'float'>  
+# <class 'complex'>
+# <class 'bool'>
+
+
+#declaring list with [] this
+# list = []
+# set = {}
+# tuple = () # we must provide at least 2 types of thins. we cannot empty this () bc of we execue this in various way .
+#Dict = {'name':'Azim Ahmed', "age":'23'} # this looks like js object
+
+li = [1, 11, 1,1]
+z = {1, 1, 2, 3}
+t = (1,1,1,1)
+d={'name':'Azim Ahmed', "age":'23'}
+print(type(li))
+print(type(z))
+print(type(t))
+print(type(d))
+
+#Output
+# <class 'list'>
+# <class 'set'>
+# <class 'tuple'>
+# <class 'dict'>
